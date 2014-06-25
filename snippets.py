68650bef74ae42ec947fb824c66386735ece748d
jdingus <josh.dingus@gmail.com>
2014-06-25 09:54:07 -0400

    Get and Put functions working from command line

--- snippets.py
+++ snippets.py
@@ -19,16 +19,20 @@ def put(name, snippet, filename):
 
 def get(name, filename):
     """ Retrieve a snippet with an associated name in the csv file """
-    with open('snippets.csv','r') as f:
+    with open(filename,'r') as f:
         reader = csv.reader(f)
-        for row in reader:            
+
+        for row in reader:
+            # snippet = row[1]            
             if row[0] == name:
-                print row[0] + ' : ' + row[1]
+                snippet = row[1]
+                print row[0] + ' * ' + snippet
+    return name, snippet, filename
 
-get('hairy', 'snippets.csv')
+# get('hairy', 'snippets.csv')
 
 
-raise SystemExit
+# raise SystemExit
 
 def make_parser():
     """ Construct the command line parser """
@@ -40,12 +44,20 @@ def make_parser():
 
     # Subparser for the put command
     logging.debug("Constructing put subparser")
-    put_parser = subparsers.add_parser("put", help="Store a snippet")
-    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser = subparsers.add_parser("put", help="Store a snippet") # Create the put_parser object
+    put_parser.add_argument("name", help="The name of the snippet") 
     put_parser.add_argument("snippet", help="The snippet text")
     put_parser.add_argument("filename", default="snippets.csv", nargs="?",
                             help="The snippet filename")
     put_parser.set_defaults(command="put")
+
+    # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
+    get_parser.add_argument("name", help="Name of the snippet to retrieve")
+    get_parser.add_argument("filename", default="snippets.csv", nargs="?",
+                            help="The snippet filename")
+    get_parser.set_defaults(command="get")
 
     return parser
 
@@ -56,11 +68,16 @@ def main():
     arguments = parser.parse_args(sys.argv[1:])
     # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
+
     command = arguments.pop("command")
 
     if command == "put":
         name, snippet = put(**arguments)
         print "Stored '{}' as '{}'".format(snippet, name)
+
+    if command == "get":
+        name, snippet, filename = get(**arguments)
+        print "Retrieved items which match: '{}' from file: '{}' .".format(name, filename)
 
 if __name__ == "__main__":
     main()
